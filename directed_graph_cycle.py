9d800d21bdd54cc4e4ddac42b942f259307466dc
geetha <reddygeetha298@gmail.com>
2026-04-27 22:08:23 +0530

    cycle detection

---
+++ directed_graph_cycle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isCyclic(self, V, edges):
+        adj=[[] for i in range(V)]
+        indeg=[0]*V
+        for u,v in edges:
+            adj[u].append(v)
+            indeg[v]+=1
+        q=[]
+        for i in range(V):
+            if indeg[i]==0:
+                q.append(i)
+        c=0
+        while q:
+            node=q.pop()
+            c+=1
+            for neig in adj[node]:
+                indeg[neig]-=1
+                if indeg[neig]==0:
+                    q.append(neig)
+        return c!=V
+                
+       
