1524831e596cb9674f94e6db1685345c18cee1c5
geetha <reddygeetha298@gmail.com>
2026-08-07 18:54:34 +0530

    patterns

--- number_patterns.py
+++ number_patterns.py
@@ -46,10 +46,10 @@ for i in range(1,n+1):
 
 
 #right triangle number pattern
-'''n=5
+n=5
 for i in range(1,n+1):
     for j in range(i-1):
         print(" ",end="")
     for j in range(2*n-(2*i-1)):
         print(j+1,end="")
-    print()'''
+    print()
