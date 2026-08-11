994d69c9ee1676ea6e1358c3e562dabca87f0a44
geetha <reddygeetha298@gmail.com>
2026-08-11 17:52:57 +0530

    finding peak elemnt

---
+++ peak_index_in_mountain.py
@@ -0,0 +1,16 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n=len(arr)
+        low=0
+        high=n-1
+        
+        while(low<high):
+            mid=(low+high)//2
+            if arr[mid]>arr[mid+1]:
+                high=mid
+            else:
+                low=mid+1
+        return low
+               
+
+        
